af58b30ee7917d42398fe188820ec0475068d138
Diego Bizerra da Silva <diegorj93@gmail.com>
2021-08-19 11:28:35 -0300

    Instancia de forma automatica da base de dados

--- bookstore/src/main/java/com/diego/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/diego/bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package com.diego.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.diego.bookstore.domain.Categoria;
+import com.diego.bookstore.domain.Livro;
+import com.diego.bookstore.repositories.CategoriaRepository;
+import com.diego.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livro de TI ");
+		
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin","lorem ipsum", cat1);
+		
+		cat1.getLivro().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
 	}
 
 }
